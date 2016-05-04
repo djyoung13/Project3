328151e87921cfa3d55359a13f6df8a386b1a617
djyoung13 <djyoung1@memphis.edu>
2016-05-04 16:38:42 -0500

    Endpoint Publishers created

---
+++ src/endpoint/NYTimesPublisher.java
@@ -0,0 +1,11 @@
+package endpoint;
+
+import javax.xml.ws.Endpoint;
+import ws.NYTimesReviewImpl;
+
+public class NYTimesPublisher {
+	public static void main(String[] args)
+	{
+		Endpoint.publish("http://localhost:8889/ws/nytimes", new NYTimesReviewImpl());
+	}
+}
---
+++ src/endpoint/PlaybillPublisher.java
@@ -0,0 +1,11 @@
+package endpoint;
+
+import javax.xml.ws.Endpoint;
+import ws.PlaybillReviewImpl;
+
+public class PlaybillPublisher {
+	public static void main(String[] args)
+	{
+		Endpoint.publish("http://localhost:8887/ws/playbill", new PlaybillReviewImpl());
+	}
+}
